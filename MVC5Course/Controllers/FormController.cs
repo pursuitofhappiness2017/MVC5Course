dd0739ea680371ab868f52c4bef3496d5eebbe61
TONY <TONY@15000651TONY>
2017-05-21 09:48:20 +0800

    練習建立 FormController 與 Edit 動作方法編輯 ProductName 欄位內容

---
+++ MVC5Course/Controllers/FormController.cs
@@ -0,0 +1,42 @@
+﻿using MVC5Course.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class FormController : Controller
+    {
+        FabricsEntities db = new FabricsEntities();
+
+        // GET: Form
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return View(db.Product.Find(id));
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id , FormCollection form)
+        {
+            var product = db.Product.Find(id);
+
+            if (TryUpdateModel(product, 
+                new string[] { "ProductName" }))
+            {
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            product = db.Product.Find(id);
+
+            return View(product);
+        }
+    }
+}
