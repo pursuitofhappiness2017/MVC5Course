e9e7c8d0acb655284ade08d61d2da71fe7c8f99f
TONY <TONY@15000651TONY>
2017-05-21 14:34:09 +0800

    練習透過 [UIHint] 自訂特性屬性的顯示範本 ( Gender & CreditRating )

--- MVC5Course/Models/Client.Partial.cs
+++ MVC5Course/Models/Client.Partial.cs
@@ -22,10 +22,12 @@ namespace MVC5Course.Models
         
         [StringLength(40, ErrorMessage="欄位長度不得大於 40 個字元")]
         public string LastName { get; set; }
-        
+
+        [UIHint("Gender")]
         [StringLength(1, ErrorMessage="欄位長度不得大於 1 個字元")]
         public string Gender { get; set; }
         public Nullable<System.DateTime> DateOfBirth { get; set; }
+        [UIHint("CreditRating")]
         public Nullable<double> CreditRating { get; set; }
         
         [StringLength(7, ErrorMessage="欄位長度不得大於 7 個字元")]
