4fdac0b7eec6fb09897903dda4c5658fd51bf544
TONY <TONY@15000651TONY>
2017-05-21 13:47:27 +0800

    練習自訂 DateTime 編輯器範本 & 顯示範本

--- MVC5Course/Controllers/Clients2Controller.cs
+++ MVC5Course/Controllers/Clients2Controller.cs
@@ -98,6 +98,10 @@ namespace MVC5Course.Controllers
             {
                 return HttpNotFound();
             }
+
+            var items = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            ViewBag.CreditRating = new SelectList(items);
+
             ViewBag.OccupationId = new SelectList(db.Occupation, "OccupationId", "OccupationName", client.OccupationId);
             return View(client);
         }
